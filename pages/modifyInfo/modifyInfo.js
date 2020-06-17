89a9a2debf6830f8c984ac300f53f205c786a182
weirukai <15623739350@163.com>
2020-06-17 10:18:45 +0800

    完成了修改信息的本地缓存

--- pages/modifyInfo/modifyInfo.js
+++ pages/modifyInfo/modifyInfo.js
@@ -1,7 +1,6 @@
 // pages/modifyInfo.js
 
-import {Toast} from "../../miniprogram_npm/vant-weapp/toast/toast"
-
+import Toast from "../../miniprogram_npm/vant-weapp/toast/toast"
 Page({
   /**
    * 页面的初始数据
@@ -18,9 +17,7 @@ Page({
       userImageSrc:"",
       School:"清华大学"
     },
-
   },
-
   showDialog:function(e)
   {
     var selected=e.target.id
@@ -41,43 +38,106 @@ Page({
   },
   bindInput:function(e)
   {
-    var inputValue=e.detail.valude
+    var inputValuetemp=e.detail.value
     this.setData({
-      inputValue:inputValue
+      inputValue:inputValuetemp
     })
   },
-
   changeGrade:function(e)
   {
+    var that=this
     wx.setStorage({
       data: this.data.inputValue,
-      key: '年级',
+      key: 'grade',
       success:res=>
       {
-        Toast.success("更改成功")
+        that.closeDialog()
+        wx.showToast({
+          title: '成功',
+        })
+        var gradePath='myInfo.grade'
+       wx.getStorage({
+         key: 'grade',
+         success:res=>{
+           this.setData({
+             [gradePath]:res.data
+           })
+         }
+       })
       }
     })
-    
-
-    
   },
   changeBookType:function(e){
-
-
+    var that=this
+    wx.setStorage({
+      data: this.data.inputValue,
+      key: 'bookType',
+      success:res=>
+      {
+        that.closeDialog()
+        wx.showToast({
+          title: '成功',
+        })
+        var bookTypePath='myInfo.bookType'
+        wx.getStorage({
+          key: 'bookType',
+          success:res=>{
+            this.setData({
+              [bookTypePath]:res.data
+            })
+          }
+        })
+      }
+    })
   },
+
+/*更改信息的点击响应，然后再调用各个信息的具体更改事件，最后刷新*/
 changeInfo:function(e)
 {
-  if(this.data.dialogTitle="年级")
+  if(this.data.dialogTitle=="年级")
   {
     this.changeGrade()
   }
+  else if(this.data.dialogTitle=="教材版本")
+  {
+    this.changeBookType()
+  }
+//更改最后应该刷新页面
 },
 
+refreshMyInfo:function()
+{
+  //跟新教材版本和年级
+  var grade=this.data.myInfo.grade
+  var bookType=this.data.myInfo.bookType
+  var gradePath='myInfo.grade'
+  var bookTypePath='myInfo.bookType'
+  wx.getStorage({
+    key: 'grade',
+    success:res=>{
+      grade=res.data
+    }
+  })
+  wx.getStorage({
+    key: 'bookType',
+    success:res=>
+    {
+      bookType=res.data
+    }
+  })
+  this.setData({
+    [bookTypePath]:bookType,
+    [gradePath]:grade
+  })
+  //跟新其他的信息，需要网络请求
+
+},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+     this.refreshMyInfo()
+    
   },
 
   /**
