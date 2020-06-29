f94286299c167f254b0c590df87272adbd0ad4d7
weirukai <15623739350@163.com>
2020-06-29 15:25:53 +0800

    更改了图片上传做测试

--- pages/Myself/Myself.js
+++ pages/Myself/Myself.js
@@ -331,9 +331,9 @@ Page({
     const { file } = event.detail;
     // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
     wx.uploadFile({
-      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+      url: myAPP.globalData.host+'/post/fileUpload', // 仅为示例，非真实的接口地址
       filePath: file.path,
-      name: 'file',
+      name: 'name',
       formData: { user: 'test' },
       success(res) {
         // 上传完成需要更新 fileList
